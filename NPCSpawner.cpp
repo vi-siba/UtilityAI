fbae18a0efddd85c7fd8009beada9034f9f5a374
vi_siba <test_mail321@mail.ru>
2025-11-04 22:16:37 +0700

    Add files via upload

---
+++ NPCSpawner.cpp
@@ -0,0 +1,92 @@
+﻿#include "ZombieSpawner.h"
+#include "Engine/Engine.h"
+#include "EngineUtils.h"
+
+AZombieSpawner::AZombieSpawner()
+{
+
+}
+
+void AZombieSpawner::SpawnNPC()
+{
+    if (SpawnPoints.IsEmpty())
+        return;
+
+    if (!ZombieClass)
+        return;
+
+    int AdditionalNPC = AdditionalQuantity();
+
+    NPCQuantity += AdditionalNPC;
+
+    for (int i = 0; i < SpawnPoints.Num(); i++)
+    {
+        for (int j = 0; j < NPCQuantity; j++)
+        {
+            int attempts = 0;
+            const int maxAttempts = 5;
+
+            while (!SpawnSingle(SpawnPoints[i]) && attempts < maxAttempts)
+            {
+                attempts++;
+            }
+        }
+    }
+
+}
+
+bool AZombieSpawner::SpawnSingle(AActor* SpawnPointActor)
+{
+    if (!SpawnPointActor)
+        return false;
+
+    FVector SpawnNPCLocation;
+
+    SpawnNPCLocation.X = SpawnPointActor->GetActorLocation().X + FMath::RandRange(-Displacement, Displacement);
+    SpawnNPCLocation.Y = SpawnPointActor->GetActorLocation().Y + FMath::RandRange(-Displacement, Displacement);
+    SpawnNPCLocation.Z = SpawnPointActor->GetActorLocation().Z;
+
+    FRotator SpawnNPCRotation = SpawnPointActor->GetActorRotation();
+
+    if (!ZombieClass)
+        return false;
+
+    AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ZombieClass, SpawnNPCLocation, SpawnNPCRotation /* SpawnParams*/);
+
+    if (SpawnedActor)
+    {
+      //  UE_LOG(LogTemp, Warning, TEXT("Actor spawned successfully"));
+        return true;
+    }
+    else
+    {
+        //  UE_LOG(LogTemp, Warning, TEXT("Actor wasn't spawned"));
+        return false;
+    }
+
+}
+
+int AZombieSpawner::AdditionalQuantity()
+{
+   int AdditionalNPCQuantity=0;
+
+   if (UtilityCurves.IsEmpty())
+       return 0;
+
+   if (CurrentParameters.IsEmpty())
+       return 0;
+
+   for (const auto& [ParamName, Curve] : UtilityCurves)
+        {
+            if (!Curve) continue;
+
+            const float* ParamValuePtr = CurrentParameters.Find(ParamName);
+            if (!ParamValuePtr) continue;
+            
+            float ParamValue = *ParamValuePtr;
+            float UtilityFromCurve = Curve->GetFloatValue(ParamValue);
+
+            AdditionalNPCQuantity += abs(UtilityFromCurve);
+        }
+   return AdditionalNPCQuantity;
+}
---
+++ NPCSpawner.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/GameplayStatics.h"
+#include "UObject/NoExportTypes.h"
+#include "Curves/CurveFloat.h"
+
+#include "HAL/CriticalSection.h"
+#include "ZombieSpawner.generated.h"
+
+
+UCLASS(Blueprintable)
+class UTILITYAI_API AZombieSpawner : public AActor
+{
+    GENERATED_BODY()
+
+public:
+
+    AZombieSpawner();
+
+    UFUNCTION(BlueprintCallable, Category = "Utility AI")
+    void SpawnNPC();
+    
+    int NPCQuantity = 5;
+    bool SpawnSingle(AActor* SpawnPointActor);
+
+    int AdditionalQuantity();
+
+    float Displacement=300.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+    TSubclassOf<AActor> ZombieClass;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Utility AI")
+    TArray<AActor*> SpawnPoints;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Utility AI")
+    TMap<FName, UCurveFloat*> UtilityCurves;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Utility AI")
+    TMap<FName, float> CurrentParameters;
+
+
+private:
+
+};
